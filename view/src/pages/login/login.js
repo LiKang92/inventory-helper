e4370ce833e04429d511b51de360204a5f4bb3ee
LiKang <kangli9233@gmail.com>
2021-12-17 22:13:40 -0500

    keep coding

--- view/src/pages/login/login.js
+++ view/src/pages/login/login.js
@@ -3,8 +3,20 @@ import { Form, Icon, Input, Button } from 'antd'
 import './css/login.less'
 import logo from '../../static/imgs/logo.png'
 
-const {Item} = Form
-const Login = () => {
+const { Item } = Form
+const Login = ({ form }) => {
+
+  const getFieldDecorator = form.getFieldDecorator
+  //不能用箭头
+  function handleSubmit() {
+
+  }
+
+  function testClick() {
+    console.log(getFieldDecorator);
+  }
+
+
   return (
     <div className='login'>
       <header>
@@ -13,28 +25,45 @@ const Login = () => {
       </header>
       <section>
         <h1>log in</h1>
-        <Form  className="login-form">
+        <Form onSubmit={handleSubmit} className="login-form">
           <Item>
-            {/* 下面的大括号代表要写js表达式了 */}
-            
+            {/* 下面的大括号代表要写js表达式了 
+            'username'代表标识，标识这个要装饰的东西
+            */}
+            {getFieldDecorator('username', {
+              rules: [
+                { required: true, message: 'Please input your username!' },
+                { max: 12, message: 'username must less then 12 digits' },
+                { min: 4, message: 'username must longer then 4 digis' },
+                { pattern: /^\w+$/, message:'username must contains number, alphbets, underscore'}
+              ],
+            })(
               <Input
                 prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                 placeholder="Username"
-              />
-            
+              />,
+            )}
+
           </Item>
           <Item>
-            
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
               <Input
                 prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                 type="password"
                 placeholder="Password"
-              />
-            
+              />,
+            )}
           </Item>
           <Item>
             <Button type="primary" htmlType="submit" className="login-form-button">
               Log in
+            </Button>
+            <Button type="primary"
+              onClick={testClick}
+              className="login-form-button">
+              test
             </Button>
           </Item>
         </Form>
@@ -43,5 +72,5 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Form.create()(Login)
 
